cb6610ca6e8e1f7aa6100b173ba5492f474ebded
Carles Rojas <carlesrojas@outlook.com>
2022-01-05 11:26:08 +0100

    React spring test

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,5 @@
-import { Transition, animated } from "react-spring";
 import "../styles/globals.scss";
 
-export default function MyApp({ Component, pageProps, router }) {
-    const items = [
-        {
-            id: router.route,
-            Component,
-            pageProps,
-        },
-    ];
-
-    return (
-        <Transition
-            items={items}
-            keys={(item) => item.id}
-            from={{ opacity: 0 }}
-            initial={{ opacity: 0 }}
-            enter={{ opacity: 1, delay: 500 }}
-            leave={{ opacity: 0, position: "absolute" }}
-        >
-            {(styles, { pageProps, Component }) => (
-                <animated.div style={{ ...styles, width: "100%" }}>
-                    <Component {...pageProps} />
-                </animated.div>
-            )}
-        </Transition>
-    );
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { useSpring, animated } from "react-spring";
 
 export default function Home() {
+    const [{ opacity }, spring] = useSpring(() => ({ opacity: "1" }));
+
     return (
         <div>
             <h1>Home</h1>
+            <div onClick={() => spring.start({ opacity: "1" })}>Show</div>
+            <div onClick={() => spring.start({ opacity: "0" })}>Hide</div>
+            <animated.div style={{ opacity, backgroundColor: "red", padding: "2rem" }}>Content</animated.div>
             <Link href="/MatchEat">MatchEat</Link>
         </div>
     );
