5382a1f837a6c9050da32901c616a98b63b26906
Carles Rojas <carlesrojas@outlook.com>
2022-01-06 13:17:38 +0100

    Slider gestures working

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useContext, memo, Fragment } from "react";
+import { useEffect, useRef, useState, useContext, memo, useCallback } from "react";
 import useClass from "../hooks/useClass";
 import SliderElem from "./SliderElem";
 import SlideButtons from "./SlideButtons";
@@ -8,88 +8,97 @@ import { Utils } from "../contexts/Utils";
 import { MediaQuery } from "../contexts/MediaQuery";
 
 const Slider = memo(({ elems, title }) => {
-    const { clamp } = useContext(Utils);
+    const { clamp, debounce } = useContext(Utils);
     const { media } = useContext(MediaQuery);
 
-    // #################################################
-    //   RESIZE
-    // #################################################
-
-    const sliderRef = useRef();
     const sliderWidth = useRef();
     const elemsInScreen = useRef();
+    const elemWidthRef = useRef();
     const [elemWidth, setElemWidth] = useState(0);
 
-    useEffect(() => {
-        const onResize = () => {
-            sliderWidth.current = window.innerWidth - (media.isMobile ? 2 * 18 : 6 * 18);
-            elemsInScreen.current = clamp(Math.floor(sliderWidth.current / 350), 1, 5);
-            setElemWidth(sliderWidth.current / elemsInScreen.current);
-        };
-
-        window.addEventListener("resize", onResize);
-        onResize();
-
-        return () => {
-            window.removeEventListener("resize", onResize);
-        };
-    }, [clamp, media, elemsInScreen]);
-
     // #################################################
-    //   SLIDE
+    //   SPRING
     // #################################################
 
-    const [{ x }, spring] = useSpring(() => ({ x: "0px" }));
-    const [, setMinMax] = useState({ min: 0, max: elemsInScreen.current - 1 });
+    const [{ x }, spring] = useSpring(() => ({ x: 0 }));
+    const [min, setMin] = useState(0);
 
-    useEffect(() => {
-        setMinMax((prev) => {
-            const newMin = prev.min;
-            const newMax = newMin + elemsInScreen.current - 1;
+    const prev = () => {
+        const currIndex = Math.floor(Math.abs(x.get()) / elemWidthRef.current);
+        const newIndex = Math.max(0, currIndex - elemsInScreen.current);
+        setMin(newIndex);
+        spring.start({ x: -newIndex * elemWidthRef.current });
+    };
 
-            if (newMax >= elems.length) {
-                newMax = elems.length - 1;
-                newMin = newMax - elemsInScreen.current + 1;
-            }
-
-            return { min: newMin, max: newMax };
-        });
-    }, [elemWidth, spring, elems]);
+    const next = () => {
+        const currIndex = Math.floor(Math.abs(x.get()) / elemWidthRef.current);
+        const newIndex = Math.min(elems.length - elemsInScreen.current, currIndex + elemsInScreen.current);
+        setMin(newIndex);
+        spring.start({ x: -newIndex * elemWidthRef.current });
+    };
 
     // #################################################
     //   GESTURE
     // #################################################
 
     const containerRef = useRef();
+    const initialX = useRef(0);
 
     // Vertical gesture
     const gestureBind = useDrag(
-        ({ event, down, offset: [ox] }) => {
+        ({ event, first, down, movement: [mx] }) => {
             event.stopPropagation();
 
+            if (first) initialX.current = x.get();
+
             if (down) {
-                spring.start({ x: `${ox}px` });
+                const maxSlide = containerRef.current.getBoundingClientRect().width - sliderWidth.current;
+                const disp = clamp(initialX.current + mx, -maxSlide, 0);
+                const currIndex = Math.floor(Math.abs(disp) / elemWidthRef.current);
+                setMin(currIndex);
+                spring.start({ x: disp });
             }
         },
         {
             filterTaps: true,
             axis: "x",
-            from: () => [x.get(), 0],
-            bounds: {
-                right: 0,
-                left: containerRef.current
-                    ? -containerRef.current.getBoundingClientRect().width + sliderWidth.current
-                    : sliderWidth.current,
-            },
         }
     );
+
+    // #################################################
+    //   RESIZE
+    // #################################################
+
+    const firstResizeDone = useRef(false);
+
+    const onResize = useCallback(() => {
+        firstResizeDone.current = true;
+        sliderWidth.current = window.innerWidth - (media.isMobile ? 2 * 18 : 6 * 18);
+        elemsInScreen.current = clamp(Math.floor(sliderWidth.current / 350), 1, 5);
+        elemWidthRef.current = sliderWidth.current / elemsInScreen.current;
+        setElemWidth(elemWidthRef.current);
+
+        const newIndex = clamp(min, 0, elems.length - elemsInScreen.current);
+        setMin(newIndex);
+        spring.start({ x: -newIndex * elemWidthRef.current, immediate: true });
+    }, [clamp, elems, media.isMobile, min, spring]);
+
+    useEffect(() => {
+        if (!firstResizeDone.current) onResize();
+        const debounceResize = debounce(onResize, 500);
+
+        window.addEventListener("resize", debounceResize);
+        return () => {
+            window.removeEventListener("resize", debounceResize);
+        };
+    }, [onResize, debounce]);
 
     // #################################################
     //   RENDER
     // #################################################
 
     return (
-        <section className={useClass("slider")} ref={sliderRef}>
+        <section className={useClass("slider")}>
             <h1 className={useClass()}>{title}</h1>
 
             <animated.div className={useClass("container")} {...gestureBind()} style={{ x }} ref={containerRef}>
@@ -98,7 +107,7 @@ const Slider = memo(({ elems, title }) => {
                 ))}
             </animated.div>
 
-            <SlideButtons next={() => {}} prev={() => {}} nextVisible={true} prevVisible={true} />
+            <SlideButtons next={next} prev={prev} nextVisible={true} prevVisible={true} />
         </section>
     );
 });
